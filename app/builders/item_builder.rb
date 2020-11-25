9c72eb705c0d04aaebb6cb574bb7b2ab86f2f24b
Hung Kieu Quoc (hungkq1724) <kieu.quoc.hung@sun-asterisk.com>
2020-11-25 09:54:19 +0700

    2020-11-25 09:54:19 +0700

--- app/builders/item_builder.rb
+++ app/builders/item_builder.rb
@@ -6,6 +6,7 @@ class ItemBuilder < BaseBuilder
 
   def build_show
     Item.all.each do |item|
+      @title = item.title
       build path: 'items/show.html.erb', locals: { item: item }, out: item.output
       item.built_status!
       BuilderFile.find_or_create_by fileable_type: Item.name, fileable_id: item.id, output: item.output
--- app/builders/tag_builder.rb
+++ app/builders/tag_builder.rb
@@ -1,6 +1,7 @@
 class TagBuilder < BaseBuilder
   def build_show
     Tag.order(name: :asc).all.each do |tag|
+      @title = "Các bài viết thuộc mục #{tag.name}"
       build path: 'tags/show.html.erb', locals: { tag: tag }, out: tag.output
       BuilderFile.find_or_create_by fileable_type: Tag.name, fileable_id: tag.id, output: tag.output
     end
